a46c910f76d57931b7a7604c481cb6d1e2b9fecc
Sachin kathar <136295822+SACHINKATHAR2005@users.noreply.github.com>
2025-08-25 02:40:32 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -54,6 +54,6 @@ app.use("/upload", uploadRouter)
 
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
